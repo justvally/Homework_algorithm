92a8e7b2b3b11aa682e91dbb42997537ae32bc5b
justvally <vateychhoung@gmail.com>
2025-10-06 13:23:17 +0000

    done

---
+++ insertanddelete_element.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void put(double *p_array, int inx, double value, int &n, int size) {
+    if (inx > n || inx >= size) {
+        cout << "Out of bound!" << endl;
+        return;
+    }
+    for (int i = n; i > inx; i--) {
+        p_array[i] = p_array[i - 1];
+    }
+    p_array[inx] = value;
+    n++;
+}
+void del(double *p_array, int inx, int &n) {
+    if (inx >= n || inx < 0) {
+        cout << "Out of bound!" << endl;
+        return;
+    }
+    for (int i = inx; i < n - 1; i++) {
+        p_array[i] = p_array[i + 1];
+    }
+    n--;
+}
+int main() {
+    double array[100] = {5, 4, 3, 2, 1};
+    int n = 5;
+    double *p_array = array;
+    put(array, 2, 10, n, 100);
+    del(array, 1, n);          
+    for (int i = 0; i < n; i++) {
+        cout << "Value: " << *(p_array + i) << endl;
+    }
+    return 0;
+}
